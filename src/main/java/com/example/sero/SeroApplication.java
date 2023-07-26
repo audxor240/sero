e57514018070434c2821a8947ba50783fe28df04
audxor240 <audxor240@naver.comn>
2023-07-26 13:58:01 +0900

    Initial commit

---
+++ src/main/java/com/example/sero/SeroApplication.java
@@ -0,0 +1,13 @@
+package com.example.sero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sero/SeroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
